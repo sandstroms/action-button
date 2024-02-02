802a165c78b45ee2f796a756cc8b44fca551199f
Shaun Sandstrom <sandstrom.shaun@gmail.com>
2024-02-01 19:22:54 -0700

    button animation is almost working, just need to make minor adjustments

--- script.js
+++ script.js
@@ -1,143 +1,53 @@
-drawButton();
+const canvas = document.getElementById("actionButton");
+const ctx = canvas.getContext("2d");
 
-function drawButton() {
-    const canvas = document.getElementById("actionButton");
-    const ctx = canvas.getContext("2d");
+drawButton(ctx);
 
-    /* Constants ------------------------------------------------------------ */
-    const FRONT_START_X = 50;
-    const FRONT_START_Y = 50;
-    const WIDTH = 200;
-    const HEIGHT = 120;
-    const ARC_RADIUS = 8;
-    const SLANT_X_LENGTH = 20;
-    const SLANT_Y_LENGTH = 20;
-    const FRONT_TOP_LEFT_CORNER = { x: FRONT_START_X, y: FRONT_START_Y };
-    const FRONT_TOP_RIGHT_CORNER = { x: FRONT_START_X + WIDTH, y: FRONT_START_Y };
-    const FRONT_BOTTOM_RIGHT_CORNER = { x: FRONT_START_X + WIDTH, y: FRONT_START_Y + HEIGHT };
-    const FRONT_BOTTOM_LEFT_CORNER = { x: FRONT_START_X, y: FRONT_START_Y + HEIGHT };
-    
-    const buttonValues = {
-        WIDTH,
-        HEIGHT,
-        ARC_RADIUS,
-        frontLayer: {
-            START_X: FRONT_START_X,
-            START_Y: FRONT_START_Y,
-            TOP_LEFT_CORNER: FRONT_TOP_LEFT_CORNER,
-            TOP_RIGHT_CORNER: FRONT_TOP_RIGHT_CORNER,
-            BOTTOM_RIGHT_CORNER: FRONT_BOTTOM_RIGHT_CORNER,
-            BOTTOM_LEFT_CORNER: FRONT_BOTTOM_LEFT_CORNER
-        },
-        intermediateLayer: {
-            TOP_RIGHT_ARC_CORNER: {
-                x: (FRONT_TOP_RIGHT_CORNER.x - ARC_RADIUS) + (Math.cos(Math.PI / 4) * ARC_RADIUS), 
-                y: (FRONT_TOP_RIGHT_CORNER.y + ARC_RADIUS) - (Math.sin(Math.PI / 4) * ARC_RADIUS)
-            },
-            BOTTOM_RIGHT_ARC_CORNER: {
-                x: (FRONT_BOTTOM_RIGHT_CORNER.x - ARC_RADIUS) + (Math.cos(Math.PI / 4) * ARC_RADIUS),
-                y: (FRONT_BOTTOM_RIGHT_CORNER.y - ARC_RADIUS) + (Math.sin(Math.PI / 4) * ARC_RADIUS)
-            },
-            BOTTOM_LEFT_ARC_CORNER: {
-                x: (FRONT_BOTTOM_LEFT_CORNER.x + ARC_RADIUS) - (Math.cos(Math.PI / 4) * ARC_RADIUS),
-                y: (FRONT_BOTTOM_LEFT_CORNER.y - ARC_RADIUS) + (Math.sin(Math.PI / 4) * ARC_RADIUS)
-            },
-            TOP_LEFT_ARC_CORNER: {
-                x: (FRONT_TOP_LEFT_CORNER.x + ARC_RADIUS) - (Math.cos(Math.PI / 4) * ARC_RADIUS),
-                y: (FRONT_TOP_LEFT_CORNER.y + ARC_RADIUS) - (Math.sin(Math.PI / 4) * ARC_RADIUS)
-            },
-            SLANT_LENGTH_X: SLANT_X_LENGTH,
-            SLANT_LENGTH_Y: SLANT_Y_LENGTH,
-            SLANT_LENGTH: Math.sqrt(Math.pow(SLANT_X_LENGTH, 2) + Math.pow(SLANT_Y_LENGTH, 2)),
-            SPACING_CONSTANT: 20
-        },
-        backgroundLayer: {
-            TOP_RIGHT_CORNER: {
-                x: FRONT_TOP_RIGHT_CORNER.x + SLANT_X_LENGTH,
-                y: FRONT_TOP_RIGHT_CORNER.y + SLANT_Y_LENGTH
-            },
-            BOTTOM_RIGHT_CORNER: {
-                x: FRONT_BOTTOM_RIGHT_CORNER.x + SLANT_X_LENGTH,
-                y: FRONT_BOTTOM_RIGHT_CORNER.y + SLANT_Y_LENGTH
-            },
-            BOTTOM_LEFT_CORNER: {
-                x: FRONT_BOTTOM_LEFT_CORNER.x + SLANT_X_LENGTH,
-                y: FRONT_BOTTOM_LEFT_CORNER.y + SLANT_Y_LENGTH
-            },
-            TOP_LEFT_CORNER: {
-                x: FRONT_TOP_LEFT_CORNER.x + SLANT_X_LENGTH,
-                y: FRONT_TOP_LEFT_CORNER.y + SLANT_Y_LENGTH
-            }
-        }
-    };
-    /* ---------------------------------------------------------------------- */
+function pressButton() {
+    clearCanvas(canvas);
+    drawPressedButton(ctx, buttonValues);
 
+    let id = null;
+    clearInterval(id);
+    id = setInterval(frame, 350);
+
+    function frame() {
+        clearCanvas(canvas);
+        drawButton(ctx);
+        clearInterval(id);
+    }
+}
+
+function clearCanvas(canvas) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawButton(ctx) {
     drawFrontLayer(ctx, buttonValues);
     drawIntermediateLayer(ctx, buttonValues);
     drawBackgroundLayer(ctx, buttonValues);
 }
 
-function drawRoundedRect(ctx, buttonValues) {
-    const { ARC_RADIUS, layer } = buttonValues;
-    const { TOP_RIGHT_CORNER, BOTTOM_RIGHT_CORNER, BOTTOM_LEFT_CORNER, TOP_LEFT_CORNER } = layer;
-
-   /* -------------- Lines --------------- */
-   ctx.beginPath();
-
-   // Top
-   ctx.moveTo(TOP_LEFT_CORNER.x + ARC_RADIUS, TOP_LEFT_CORNER.y);
-   ctx.lineTo(TOP_RIGHT_CORNER.x - ARC_RADIUS, TOP_RIGHT_CORNER.y);
-
-   // Right
-   ctx.moveTo(TOP_RIGHT_CORNER.x, TOP_RIGHT_CORNER.y + ARC_RADIUS);
-   ctx.lineTo(BOTTOM_RIGHT_CORNER.x, BOTTOM_RIGHT_CORNER.y - ARC_RADIUS);
-
-   // Bottom
-   ctx.moveTo(BOTTOM_RIGHT_CORNER.x - ARC_RADIUS, BOTTOM_RIGHT_CORNER.y);
-   ctx.lineTo(BOTTOM_LEFT_CORNER.x + ARC_RADIUS, BOTTOM_LEFT_CORNER.y);
-
-   // Left
-   ctx.moveTo(BOTTOM_LEFT_CORNER.x, BOTTOM_LEFT_CORNER.y - ARC_RADIUS);
-   ctx.lineTo(TOP_LEFT_CORNER.x, TOP_LEFT_CORNER.y + ARC_RADIUS);
-
-   ctx.stroke()
-   /* ------------------------------------- */
-
-   /* -------------- Arcs ----------------- */
-   // Top-right
-   ctx.beginPath();
-   ctx.arc(TOP_RIGHT_CORNER.x - ARC_RADIUS, TOP_RIGHT_CORNER.y + ARC_RADIUS, ARC_RADIUS, 3 * Math.PI / 2, 0);
-   ctx.stroke();
-
-   // Bottom-right
-   ctx.beginPath();
-   ctx.arc(BOTTOM_RIGHT_CORNER.x - ARC_RADIUS, BOTTOM_RIGHT_CORNER.y - ARC_RADIUS, ARC_RADIUS, 0, Math.PI / 2);
-   ctx.stroke();
-
-   // Bottom-left
-   ctx.beginPath();
-   ctx.arc(BOTTOM_LEFT_CORNER.x + ARC_RADIUS, BOTTOM_LEFT_CORNER.y - ARC_RADIUS, ARC_RADIUS, Math.PI / 2, Math.PI);
-   ctx.stroke();
- 
-   // Top-left
-   ctx.beginPath();
-   ctx.arc(TOP_LEFT_CORNER.x + ARC_RADIUS, TOP_LEFT_CORNER.y + ARC_RADIUS, ARC_RADIUS, Math.PI, 3 * Math.PI / 2);
-   ctx.stroke();
-   /* ------------------------------------- */
+function drawPressedButton(ctx) {
+    drawFrontLayer(ctx, pressedButtonValues);
+    drawIntermediateLayer(ctx, pressedButtonValues);
+    drawBackgroundLayer(ctx, pressedButtonValues);
 }
 
 function drawFrontLayer(ctx, buttonValues) {
-    const { WIDTH, HEIGHT, frontLayer } = buttonValues;
-    const { START_X, START_Y } = frontLayer;
+    const frontLayer = buttonValues.frontLayer;
 
-    drawRoundedRect(ctx, { layer: frontLayer, ...buttonValues });
+    drawTopLine(ctx, { layer: frontLayer, ...buttonValues });
+    drawRightLine(ctx, { layer: frontLayer, ...buttonValues });
+    drawBottomLine(ctx, { layer: frontLayer, ...buttonValues });
+    drawLeftLine(ctx, { layer: frontLayer, ...buttonValues });
 
-    /* -------------- Text ----------------- */
-    ctx.font = "30px Arial";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillText("Submit", (WIDTH / 2) + START_X, (HEIGHT / 2) + START_Y);
-    /* ------------------------------------- */
+    drawTopRightArc(ctx, { layer: frontLayer, ...buttonValues });
+    drawBottomRightArc(ctx, { layer: frontLayer, ...buttonValues });
+    drawBottomLeftArc(ctx, { layer: frontLayer, ...buttonValues });
+    drawTopLeftArc(ctx, { layer: frontLayer, ...buttonValues });
+
+    drawText(ctx, { layer: frontLayer, ...buttonValues });
 }
 
 function drawIntermediateLayer(ctx, buttonValues) {
@@ -152,30 +62,16 @@ function drawIntermediateLayer(ctx, buttonValues) {
     ctx.moveTo(TOP_RIGHT_ARC_CORNER.x, TOP_RIGHT_ARC_CORNER.y);
     ctx.lineTo(TOP_RIGHT_ARC_CORNER.x + SLANT_LENGTH_X, TOP_RIGHT_ARC_CORNER.y + SLANT_LENGTH_Y);
  
-    // Bottom-right
-    ctx.moveTo(BOTTOM_RIGHT_ARC_CORNER.x, BOTTOM_RIGHT_ARC_CORNER.y);
-    ctx.lineTo(BOTTOM_RIGHT_ARC_CORNER.x + SLANT_LENGTH_X, BOTTOM_RIGHT_ARC_CORNER.y + SLANT_LENGTH_Y);
- 
     // Bottom-left
     ctx.moveTo(BOTTOM_LEFT_ARC_CORNER.x, BOTTOM_LEFT_ARC_CORNER.y);
     ctx.lineTo(BOTTOM_LEFT_ARC_CORNER.x + SLANT_LENGTH_X, BOTTOM_LEFT_ARC_CORNER.y + SLANT_LENGTH_Y);
  
-    // Top-left
-    ctx.moveTo(TOP_LEFT_ARC_CORNER.x, TOP_LEFT_ARC_CORNER.y);
-    ctx.lineTo(TOP_LEFT_ARC_CORNER.x + SLANT_LENGTH_X, TOP_LEFT_ARC_CORNER.y + SLANT_LENGTH_Y);
-
     ctx.stroke()
     /* ------------------------------------- */
 
     /* Lines in-between corners --- */
     ctx.strokeStyle = "red";
     ctx.beginPath();
-
-    // Top
-    for (let i = SPACING_CONSTANT; i < WIDTH; i += SPACING_CONSTANT) {
-        ctx.moveTo(TOP_LEFT_CORNER.x + i, TOP_LEFT_CORNER.y);
-        ctx.lineTo(TOP_LEFT_CORNER.x + SLANT_LENGTH_X + i, TOP_LEFT_CORNER.y + SLANT_LENGTH_Y);
-    }
 
     // Right
     for (let i = SPACING_CONSTANT; i < HEIGHT; i += SPACING_CONSTANT) {
@@ -189,11 +85,7 @@ function drawIntermediateLayer(ctx, buttonValues) {
         ctx.lineTo(BOTTOM_RIGHT_CORNER.x + SLANT_LENGTH_X - i, BOTTOM_RIGHT_CORNER.y + SLANT_LENGTH_Y);
     }
 
-    // Left
-    for (let i = SPACING_CONSTANT; i < HEIGHT; i += SPACING_CONSTANT) {
-        ctx.moveTo(BOTTOM_LEFT_CORNER.x, BOTTOM_LEFT_CORNER.y - i);
-        ctx.lineTo(BOTTOM_LEFT_CORNER.x + SLANT_LENGTH_X, BOTTOM_LEFT_CORNER.y + SLANT_LENGTH_Y - i);
-    }
+    drawBottomRightCornerLines(ctx, { layer: buttonValues.intermediateLayer, ...buttonValues });
 
     ctx.stroke();
     ctx.strokeStyle = "black";
@@ -201,5 +93,123 @@ function drawIntermediateLayer(ctx, buttonValues) {
 }
 
 function drawBackgroundLayer(ctx, buttonValues) {
-    drawRoundedRect(ctx, { layer: buttonValues.backgroundLayer, ...buttonValues });
+    const backgroundLayer = buttonValues.backgroundLayer;
+
+    drawRightLine(ctx, { layer: backgroundLayer, ...buttonValues });
+    drawBottomLine(ctx, { layer: backgroundLayer, ...buttonValues });
+
+    drawTopRightHalfArc(ctx, { layer: backgroundLayer, ...buttonValues });
+    drawBottomRightArc(ctx, { layer: backgroundLayer, ...buttonValues });
+    drawBottomLeftHalfArc(ctx, { layer: backgroundLayer, ...buttonValues });
+}
+
+function drawTopLine(ctx, buttonValues) {
+    const { ARC_RADIUS, layer } = buttonValues;
+    const { TOP_LEFT_CORNER, TOP_RIGHT_CORNER }  = layer;
+
+    ctx.beginPath();
+    ctx.moveTo(TOP_LEFT_CORNER.x + ARC_RADIUS, TOP_LEFT_CORNER.y);
+    ctx.lineTo(TOP_RIGHT_CORNER.x - ARC_RADIUS, TOP_RIGHT_CORNER.y);
+    ctx.stroke();
+}
+
+function drawRightLine(ctx, buttonValues) {
+    const { ARC_RADIUS, layer } = buttonValues;
+    const { TOP_RIGHT_CORNER, BOTTOM_RIGHT_CORNER }  = layer;
+
+    ctx.beginPath();
+    ctx.moveTo(TOP_RIGHT_CORNER.x, TOP_RIGHT_CORNER.y + ARC_RADIUS);
+    ctx.lineTo(BOTTOM_RIGHT_CORNER.x, BOTTOM_RIGHT_CORNER.y - ARC_RADIUS);
+    ctx.stroke();
+}
+
+function drawBottomLine(ctx, buttonValues) {
+    const { ARC_RADIUS, layer } = buttonValues;
+    const { BOTTOM_LEFT_CORNER, BOTTOM_RIGHT_CORNER }  = layer;
+
+    ctx.beginPath();
+    ctx.moveTo(BOTTOM_RIGHT_CORNER.x - ARC_RADIUS, BOTTOM_RIGHT_CORNER.y);
+    ctx.lineTo(BOTTOM_LEFT_CORNER.x + ARC_RADIUS, BOTTOM_LEFT_CORNER.y);
+    ctx.stroke();
+}
+
+function drawLeftLine(ctx, buttonValues) {
+    const { ARC_RADIUS, layer } = buttonValues;
+    const { BOTTOM_LEFT_CORNER, TOP_LEFT_CORNER }  = layer;
+
+    ctx.beginPath();
+    ctx.moveTo(BOTTOM_LEFT_CORNER.x, BOTTOM_LEFT_CORNER.y - ARC_RADIUS);
+    ctx.lineTo(TOP_LEFT_CORNER.x, TOP_LEFT_CORNER.y + ARC_RADIUS);
+    ctx.stroke();
+}
+
+function drawTopRightArc(ctx, buttonValues) {
+    const { ARC_RADIUS, layer } = buttonValues;
+    const { TOP_RIGHT_CORNER }  = layer;
+
+    ctx.beginPath();
+    ctx.arc(TOP_RIGHT_CORNER.x - ARC_RADIUS, TOP_RIGHT_CORNER.y + ARC_RADIUS, ARC_RADIUS, 3 * Math.PI / 2, 0);
+    ctx.stroke();
+}
+
+function drawBottomRightArc(ctx, buttonValues) {
+    const { ARC_RADIUS, layer } = buttonValues;
+    const { BOTTOM_RIGHT_CORNER }  = layer;
+
+    ctx.beginPath();
+    ctx.arc(BOTTOM_RIGHT_CORNER.x - ARC_RADIUS, BOTTOM_RIGHT_CORNER.y - ARC_RADIUS, ARC_RADIUS, 0, Math.PI / 2);
+    ctx.stroke(); 
+}
+
+function drawBottomLeftArc(ctx, buttonValues) {
+    const { ARC_RADIUS, layer } = buttonValues;
+    const { BOTTOM_LEFT_CORNER }  = layer;
+
+    ctx.beginPath();
+    ctx.arc(BOTTOM_LEFT_CORNER.x + ARC_RADIUS, BOTTOM_LEFT_CORNER.y - ARC_RADIUS, ARC_RADIUS, Math.PI / 2, Math.PI);
+    ctx.stroke();
+}
+
+function drawTopLeftArc(ctx, buttonValues) {
+    const { ARC_RADIUS, layer } = buttonValues;
+    const { TOP_LEFT_CORNER }  = layer;
+
+    ctx.beginPath();
+    ctx.arc(TOP_LEFT_CORNER.x + ARC_RADIUS, TOP_LEFT_CORNER.y + ARC_RADIUS, ARC_RADIUS, Math.PI, 3 * Math.PI / 2);
+    ctx.stroke();
+}
+
+function drawText(ctx, buttonValues) {
+    const { WIDTH, HEIGHT, layer } = buttonValues;
+    const { START_X, START_Y } = layer;
+
+    ctx.font = "30px Arial";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText("Submit", (WIDTH / 2) + START_X, (HEIGHT / 2) + START_Y);
+}
+
+function drawBottomLeftHalfArc(ctx, buttonValues) {
+    const { ARC_RADIUS, layer } = buttonValues;
+    const { BOTTOM_LEFT_CORNER }  = layer;
+
+    ctx.beginPath();
+    ctx.arc(BOTTOM_LEFT_CORNER.x + ARC_RADIUS, BOTTOM_LEFT_CORNER.y - ARC_RADIUS, ARC_RADIUS, Math.PI / 2, 3 * Math.PI / 4);
+    ctx.stroke();
+}
+
+function drawTopRightHalfArc(ctx, buttonValues) {
+    const { ARC_RADIUS, layer } = buttonValues;
+    const { TOP_RIGHT_CORNER }  = layer;
+
+    ctx.beginPath();
+    ctx.arc(TOP_RIGHT_CORNER.x - ARC_RADIUS, TOP_RIGHT_CORNER.y + ARC_RADIUS, ARC_RADIUS, 7 * Math.PI / 4, 0);
+    ctx.stroke();
+}
+
+function drawBottomRightCornerLines(ctx, buttonValues) {
+    const { BOTTOM_RIGHT_ARC_CORNER, SLANT_LENGTH_X, SLANT_LENGTH_Y } = buttonValues.intermediateLayer;
+
+    ctx.moveTo(BOTTOM_RIGHT_ARC_CORNER.x, BOTTOM_RIGHT_ARC_CORNER.y);
+    ctx.lineTo(BOTTOM_RIGHT_ARC_CORNER.x + SLANT_LENGTH_X, BOTTOM_RIGHT_ARC_CORNER.y + SLANT_LENGTH_Y);
 }
